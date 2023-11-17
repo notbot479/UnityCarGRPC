7e72d1b21380a940a38ccb1514be3a922b32575f
Max <max479dev@gmail.com>
2023-11-17 18:16:01 +0600

    test grpc and yetanotherhttphandler on trunk project [fixes needed]

--- trunk/Assets/grpc.cs
+++ trunk/Assets/grpc.cs
@@ -7,23 +7,26 @@ using System.Net.Http;
 using Grpc.Net.Client;
 using Grpc.Core;
 
+using GreeterServiceApp;
+
 public class grpc : MonoBehaviour
 {
     // Start is called before the first frame update
     async void Start()
     {
         // http test
-        using var handler = new YetAnotherHttpHandler();
-        var client = new HttpClient(handler);
-        var response = await client.GetStringAsync("https://http2.pro/");
-        Debug.Log(response);
+        //using var handler = new YetAnotherHttpHandler();
+        //var client = new HttpClient(handler);
+        //var response = await client.GetStringAsync("https://http2.pro/");
+        //Debug.Log(response);
 
         // grpc test
-        //using var handler = new YetAnotherHttpHandler();
-        //using var channel = GrpcChannel.ForAddress("http://localhost:50051/Greeter/SayHello", new GrpcChannelOptions() { HttpHandler = handler });
-        //var greeter = new GreeterClient(channel);
-        //var result = await greeter.SayHelloAsync(new HelloRequest { Name = "Alice" });
-        //Debug.Log(result);
+        using var handler = new YetAnotherHttpHandler();
+        using var channel = GrpcChannel.ForAddress("http://localhost:50051/Greeter/SayHello", new GrpcChannelOptions() { HttpHandler = handler });
+        Debug.Log(channel);
+        var greeter = new Greeter.GreeterClient(channel);
+        var result = await greeter.SayHelloAsync(new HelloRequest { Name = "Alice" });
+        Debug.Log(result);
     }
 
     void Update()
