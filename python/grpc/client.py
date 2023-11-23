2620e64ca214ed0608a328bcf88c42472d020ec8
Max <avdeenkomaxim123@gmail.com>
2023-11-23 17:17:06 +0600

    fix - update python grpc, change responce message

--- python/grpc/client.py
+++ python/grpc/client.py
@@ -6,7 +6,7 @@ from Protos import greet_pb2_grpc as pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:5281') as channel:
         client = pb2_grpc.GreeterStub(channel)
-        response = client.SayHello(pb2.HelloRequest(name='Some'))
+        response = client.SayHello(pb2.HelloRequest(name='World'))
     print("Greeter client received: " + response.message)
 
 if __name__ == '__main__':
--- python/grpc/server.py
+++ python/grpc/server.py
@@ -6,14 +6,16 @@ import grpc
 
 class GreeterServicer(pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
-        return pb2.HelloReply(message=f'Hi!')
+        name = request.name
+        return pb2.HelloReply(message=f'Hello {name}')
 
 def serve():
+    port = 5281
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
-    print("Server started. Listening on port 50051.")
+    print(f"Server started. Listening on port {port}")
     server.wait_for_termination()
 
 if __name__ == '__main__':
