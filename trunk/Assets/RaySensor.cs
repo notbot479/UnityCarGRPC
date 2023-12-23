4a0ab872272536d550ff78a149e9e7368902037f
dantepalacio <5562707@mail.ru>
2023-12-23 11:46:12 +0600

    feat(distance_sensors) - create 3 on front and 3 on back distance sensors, update car prefab

--- trunk/Assets/RaySensor.cs
+++ trunk/Assets/RaySensor.cs
@@ -1,50 +1,25 @@
 using UnityEngine;
-
 public class RaySensor : MonoBehaviour
 {
-    public float sensorLength = 10f;
-    public float sensorSpread = 30f;
-    public Color rayColor = Color.red;
+    public string sensorName = "Sensor";
+    public bool sensorShow = true;
+    public Color rayColor = Color.blue;
 
     void Update()
     {
-        // Cast rays on the front sensors
-        CastSensorRays(transform.forward, Vector3.forward);
-
-        // Cast rays on the back sensors
-        CastSensorRays(-transform.forward, Vector3.back);
-    }
-
-    void CastSensorRays(Vector3 direction, Vector3 sensorDirection)
-    {
-        for (int i = -1; i <= 1; i++)
+        // init ray
+        var origin = transform.position;
+        var direction = transform.right;        
+        Ray ray = new Ray(origin, direction);
+        // show rays in debug mode
+        if (sensorShow) {Debug.DrawRay(origin, direction * 100f, rayColor);}
+        // check collision
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
         {
-            // Calculate the angle for the current sensor
-            float angle = i * sensorSpread;
-
-            // Rotate the sensor direction based on the angle
-            Vector3 rotatedDirection = Quaternion.Euler(0, angle, 0) * sensorDirection;
-
-            // Cast a ray in the rotated direction
-            Ray ray = new Ray(transform.position, direction + rotatedDirection);
-            RaycastHit hit;
-
-            // Check for collisions
-            if (Physics.Raycast(ray, out hit, sensorLength))
-            {
-                Debug.DrawLine(ray.origin, hit.point, rayColor);
-                
-                // You can access the distance to the object with hit.distance
-                float distanceToObstacle = hit.distance;
-
-                // Do something with the distance information (e.g., apply brakes based on proximity)
-            }
-            else
-            {
-                // Draw the full sensor length if no collision
-                Debug.DrawRay(ray.origin, ray.direction * sensorLength, rayColor);
-            }
+            Debug.Log(sensorName+": "+hit.distance.ToString());
         }
     }
+
 }
 
