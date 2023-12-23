cbdf9b69d90af8e26fab8ad22b5c17c34139f5e7
dantepalacio <5562707@mail.ru>
2023-12-23 12:47:06 +0600

    feat(distance_sensors) - rewrite code, add max distance raycast and debug

---
+++ trunk/Assets/Scripts/RaySensor.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+public class RaySensor : MonoBehaviour
+{
+    private readonly float maxDistanceRaycast = float.PositiveInfinity;
+    private readonly float maxDistanceDebug = 1000f;
+    public string sensorName = "Sensor";
+    public Color rayColor = Color.blue;
+    public bool sensorShow = true;
+
+    void Update()
+    {
+        // init ray
+        var origin = transform.position;
+        var direction = transform.right;
+        Ray ray = new Ray(origin, direction);
+        // show rays in debug mode
+        if (sensorShow) { Debug.DrawRay(origin, direction * maxDistanceDebug, rayColor); }
+        // check collision with all objects
+        RaycastHit hit;
+        float distanceToTarget = maxDistanceRaycast;
+        if (Physics.Raycast(ray,out hit, maxDistanceRaycast)) { 
+            distanceToTarget = hit.distance; 
+        }
+        Debug.Log("["+sensorName+"] Distance: " + distanceToTarget.ToString());
+    }
+
+}
