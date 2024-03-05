b00d49fa0c84934a4e7f4fe9ac977995f9f82630
Max <avdeenkomaxim123@gmail.com>
2024-03-05 12:41:12 +0500

    feat(dqn) - init dqn agent

---
+++ python/grpc/car_communication/dqn/agent.py
@@ -0,0 +1,176 @@
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.layers import (
+    MaxPooling2D, 
+    Activation, 
+    Flatten,
+    Dropout, 
+    Conv2D, 
+    Dense, 
+)
+import tensorflow as tf
+
+from collections import deque
+import numpy as np
+import random
+import time
+
+from config import *
+
+
+class DQNAgent:
+    def __init__(self):
+        # Main model
+        self.model = self.create_model()
+        # Target network
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+        # Array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        # Custom tensorboard object
+        tm = int(time.time())
+        log_dir = f"logs/{MODEL_NAME}-{tm}"
+        self.tensorboard = ModifiedTensorBoard(log_dir=log_dir)
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
+
+    def create_model(self):
+        # TODO create own model
+        
+        model = Model()
+
+        # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
+        model.add(Conv2D(256, (3, 3), input_shape=(10,10,3)))  
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())  
+        model.add(Dense(64))
+
+        # ACTION_SPACE_SIZE = how many choices (9)
+        model.add(Dense(9, activation='linear'))  
+        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        return model
+
+    def update_replay_memory(self, transition):
+        '''
+        Adds step's data to a memory replay array
+        (old_state, action, reward, new_state, done)
+        '''
+        self.replay_memory.append(transition)
+
+    def train(self, terminal_state, step):
+        '''Trains main network every step during episode'''
+        # Start training only if certain number of samples is already saved
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE: return
+        # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # Get current states from minibatch, then query NN model for Q values
+        current_states = np.array([transition[0] for transition in minibatch])/255 #pyright: ignore
+        current_qs_list = self.model.predict(current_states)
+
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
+        new_current_states = np.array([transition[3] for transition in minibatch])/255 #pyright: ignore
+        future_qs_list = self.target_model.predict(new_current_states)
+        X, y = [], []
+
+        # Now we need to enumerate our batches
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            # Update Q value for given state
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
+
+        # Fit on all samples as one batch, log only on terminal state
+        callbacks = [self.tensorboard] if terminal_state else None
+        self.model.fit(
+            np.array(X)/255, #pyright: ignore
+            np.array(y), 
+            batch_size=MINIBATCH_SIZE, 
+            verbose=0, 
+            shuffle=False, 
+            callbacks=callbacks,
+        )
+
+        # Update target network counter every episode
+        if terminal_state: self.target_update_counter += 1
+        # If counter reaches set value, update target network with weights of main network
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    def get_qs(self, state):
+        '''
+        Queries main network for Q values given current observation space (environment state)
+        '''
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+
+class ModifiedTensorBoard(TensorBoard):
+    def __init__(self, **kwargs):
+        '''
+        Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+        '''
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    def set_model(self, model):
+        '''
+        Overriding this method to stop creating default log writer
+        '''
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        '''
+        Overrided, saves logs with our step number
+        (otherwise every .fit() will start writing from 0th step)
+        '''
+        self.update_stats(**logs)
+
+    def on_batch_end(self, batch, logs=None):
+        '''
+        Overrided
+        We train for one batch only, no need to save anything at epoch end
+    
+        '''
+        pass
+
+    def on_train_end(self, _):
+        '''Overrided, so won't close writer'''
+        pass
+ 
+    def update_stats(self, **stats):
+        '''
+        Custom method for saving own metrics
+        Creates writer, writes custom metrics and closes writer
+        '''
+        self._write_logs(stats, self.step)
+
+
+def _test():
+    agent = DQNAgent()
+
+if __name__ == '__main__':
+    _test()
