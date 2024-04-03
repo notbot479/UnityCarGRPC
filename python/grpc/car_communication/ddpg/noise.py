5e78cecf9d30a23a6c315a566a2e6e744a0191f5
Max <avdeenkomaxim123@gmail.com>
2024-04-03 21:59:15 +0500

    fix: update ddpg noise class from keras examples

--- python/grpc/car_communication/ddpg/noise.py
+++ python/grpc/car_communication/ddpg/noise.py
@@ -2,19 +2,34 @@ import numpy as np
 
 
 class OUNoise:
-    '''Define Ornstein-Uhlenbeck Process for Exploration Noise'''
-    def __init__(self, action_dim:int, mu:float=0, theta:float=0.15, sigma:float=0.2):
-        self.action_dim = action_dim
-        self.mu = mu
+    def __init__(
+        self, 
+        action_dim: int, 
+        std_deviation:float = 0.2, 
+        theta:float = 0.15, 
+        dt:float = 1e-2, 
+        x_initial=None
+    ) -> None:
         self.theta = theta
-        self.sigma = sigma
+        self.mean = np.zeros(action_dim)
+        self.std_dev = std_deviation
+        self.dt = dt
+        self.x_initial = x_initial
         self.reset()
 
-    def reset(self):
-        self.state = np.ones(self.action_dim) * self.mu
+    def sample(self) -> np.ndarray:
+        # Formula taken from https://www.wikipedia.org/wiki/Ornstein-Uhlenbeck_process
+        x = (
+            self.x_prev
+            + self.theta * (self.mean - self.x_prev) * self.dt
+            + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape)
+        )
+        # Store x into x_prev, makes next noise dependent on current one
+        self.x_prev = x
+        return x
 
-    def sample(self):
-        x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
-        self.state = x + dx
-        return self.state
+    def reset(self) -> None:
+        if self.x_initial is not None:
+            self.x_prev = self.x_initial
+        else:
+            self.x_prev = np.zeros_like(self.mean)
