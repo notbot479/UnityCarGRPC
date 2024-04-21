394ca7b1329a8c50e55fda6ccdfec8fa6629c3d7
Max <avdeenkomaxim123@gmail.com>
2024-04-21 21:16:53 +0500

    fix: simplify critic model

--- python/grpc/car_communication/ddpg/critic.py
+++ python/grpc/car_communication/ddpg/critic.py
@@ -6,7 +6,7 @@ from .basemodel import BaseModel
 
 
 class CriticModel(BaseModel):
-    _concat_tensor = 7 * 8
+    _concat_tensor = 5 * 8
 
     def __init__(
         self, 
@@ -21,8 +21,8 @@ class CriticModel(BaseModel):
         }
         super().__init__(**params)
 
-        self.concat_fc = nn.Linear(self._concat_tensor, self._concat_tensor)
-        self.concat_bn = nn.BatchNorm1d(self._concat_tensor)
+        self.concat_fc = nn.Linear(self._concat_tensor, 64)
+        self.concat_bn = nn.BatchNorm1d(64)
 
         self._init_actor_action_nn()
         self._init_concat_nn()
@@ -34,13 +34,9 @@ class CriticModel(BaseModel):
         # car sensors [0, 1]
         image: Tensor, 
         distance_sensors_distances: Tensor, 
-        nearest_routers: Tensor,
         distance_to_target_router: Tensor,
-        distance_to_box: Tensor, 
         # car hints [0 or 1]
         in_target_area: Tensor, 
-        boxes_is_found: Tensor, 
-        target_found: Tensor,
         #critic input [-1, 1]
         actor_action: Tensor, 
         *args,**kwargs #pyright: ignore
@@ -50,17 +46,10 @@ class CriticModel(BaseModel):
         x_distance = self.distance_to_input(
             distance_sensors_distances=distance_sensors_distances,
         )
-        x_routers = self.routers_to_input(nearest_routers=nearest_routers)
         x_speed = self.speed_to_input(speed=speed)
         x_stage1 = self.stage1_to_input(
             in_target_area=in_target_area,
             distance_to_target_router=distance_to_target_router,
-        )
-        x_stage2 = self.stage2_to_input(
-            in_target_area=in_target_area,
-            boxes_is_found=boxes_is_found,
-            target_found=target_found,
-            distance_to_box=distance_to_box,
         )
         x_actor = self.actor_action_to_input(actor_action=actor_action)
 
@@ -68,10 +57,8 @@ class CriticModel(BaseModel):
         concat = self.inputs_to_concat(
             x_image=x_image,
             x_distance=x_distance,
-            x_routers=x_routers,
             x_speed=x_speed,
             x_stage1=x_stage1,
-            x_stage2=x_stage2,
             x_actor=x_actor,
         )
         q = self.concat_to_q(concat=concat)
@@ -96,7 +83,7 @@ class CriticModel(BaseModel):
         concat: Tensor,
         *,
         prefix:str='concat',
-        count:int=4,
+        count:int=3,
     ) -> Tensor:
         x = self.forward_linear_block(
             input_tensor=concat,
@@ -111,13 +98,11 @@ class CriticModel(BaseModel):
         self,
         x_image: Tensor,
         x_distance: Tensor,
-        x_routers: Tensor,
         x_speed: Tensor,
         x_stage1: Tensor,
-        x_stage2: Tensor,
         x_actor: Tensor,
     ) -> Tensor:
-        _c = [x_image, x_distance, x_routers, x_speed, x_stage1, x_stage2, x_actor]
+        _c = [x_image, x_distance, x_speed, x_stage1, x_actor]
         x = torch.cat(_c, dim=1)
         # add batch normalization
         x = self.concat_fc(x)
@@ -126,11 +111,10 @@ class CriticModel(BaseModel):
         return x
 
 
-    def _init_concat_nn(self, input_dim:int = 7 * 8, output_dim:int = 8) -> None:
-        self.concat_fc1 = nn.Linear(input_dim, 512)
-        self.concat_fc2 = nn.Linear(512, 512)
-        self.concat_fc3 = nn.Linear(512, 256)
-        self.concat_fc4 = nn.Linear(256, output_dim)
+    def _init_concat_nn(self, input_dim:int = 64, output_dim:int = 32) -> None:
+        self.concat_fc1 = nn.Linear(input_dim, 128)
+        self.concat_fc2 = nn.Linear(128, 64)
+        self.concat_fc3 = nn.Linear(64, output_dim)
 
         self.concat_fc_out = nn.Linear(output_dim, 1)
 
